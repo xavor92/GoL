e0391c616cce857cedfac8e96c73b7049f1b4919
Olli <//>
2016-01-06 14:33:55 +0100

    - added __str__ function

--- GoL/src/GoL.py
+++ GoL/src/GoL.py
@@ -7,6 +7,15 @@ class GoL(object):
         self._playfield = [[0 for _i in range(ydim)] for _i in range(xdim)]
         self._stepCount = 0
         
+    def __str__(self, *args, **kwargs):
+        header = "Game of Life\nStep {0}\n".format(self._stepCount)
+        representation = ""
+        for row in self._playfield:
+            for value in row:
+                representation += str(value) + " "
+            representation += '\n'
+        return header + representation
+        
     def getStep(self):
         return self._stepCount
     
